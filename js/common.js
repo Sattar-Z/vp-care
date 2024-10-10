172b28b7b6166371f49777f1338bf2cc3057860c
Sattar <aasattar@icloud.com>
2024-10-10 10:14:14 +0100

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,22 @@
+// Function to load external HTML content
+function loadHTML(selector, file) {
+  fetch(file)
+    .then((response) => {
+      if (response.ok) {
+        return response.text();
+      } else {
+        throw new Error("Failed to load the file: " + file);
+      }
+    })
+    .then((data) => {
+      document.querySelector(selector).innerHTML = data;
+    })
+    .catch((error) => {
+      console.error("Error loading the file:", error);
+    });
+}
+
+// Load header and footer
+loadHTML("head", "head.html");
+loadHTML("header", "header.html");
+loadHTML("footer", "footer.html");
